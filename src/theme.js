9a59350182aba938ad6fb802978350d838d9b420
Indisputable09 <sainchyksasha2015999@gmail.com>
2022-09-25 13:25:54 +0300

    Added images and spaces

--- src/theme.js
+++ src/theme.js
@@ -3,39 +3,26 @@ export const theme = {
     primary: '#F4E041',
     secondary: '#00BDD3',
     background: '#F8F8F8',
+    hover: '#FFE302',
+    disabled: '#B4B4B4',
   },
-  space: [0, 2, 4, 8, 20, 32, 64, 128, 256, 'auto'],
-  fonts: {
-    body: 'system-ui, sans-serif',
-    heading: 'system-ui, sans-serif',
-    monospace: 'Menlo, monospace',
-  },
+  space: [0, 2, 4, 16, 20, 32, 60, 128, 256, 'auto'],
   fontSizes: {
-    xs: '12px',
-    s: '14px',
     m: '16px',
-    l: '32px',
-    xl: '64px',
-  },
-  fontWeights: {
-    normal: 400,
-    bold: 700,
+    l: '40px',
   },
   lineHeights: {
-    body: 1.5,
-    heading: 1.125,
+    body: 1.6,
+    heading: 1,
   },
-  letterSpacings: {
-    normal: 'normal',
-  },
-  opacities: {
-    none: 0,
-  },
-  shadows: {
-    main: '0 7px 10px rgba(0, 0, 0, 0.4)',
-    cardHover:
-      '0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)',
-  },
+  // opacities: {
+  //   none: 0,
+  // },
+  // shadows: {
+  //   main: '0 7px 10px rgba(0, 0, 0, 0.4)',
+  //   cardHover:
+  //     '0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)',
+  // },
   transition: {
     transform: 'transform 300ms cubic-bezier(0.4, 0, 0.2, 1)',
     boxShadow: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1)',

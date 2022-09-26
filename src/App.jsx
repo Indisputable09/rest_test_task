988475bcedc8df93f825b3665c3b333e889e5979
Indisputable09 <sainchyksasha2015999@gmail.com>
2022-09-26 13:40:17 +0300

    Started header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { GlobalStyle } from 'components/GlobalStyle';
+import Header from 'Header/Header'
 
 export const App = () => {
   return (
     <>
       <GlobalStyle />
       <div>React homework template</div>
+      <Header/>
     </>
   );
 };
